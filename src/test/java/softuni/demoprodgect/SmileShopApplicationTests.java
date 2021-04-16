ee228918166023e4c98d77e463eff66d1cf16a16
Yordanka Daskalova <45321604+Yordanka83@users.noreply.github.com>
2021-04-16 23:37:41 +0300

    Add files via upload

---
+++ src/test/java/softuni/demoprodgect/SmileShopApplicationTests.java
@@ -0,0 +1,13 @@
+package softuni.smileShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmileShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
